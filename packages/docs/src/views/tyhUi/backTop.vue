74af40d59fcb588de6d5df149430f6cab564f14d
bob <301085344@qq.com>
2022-06-13 15:49:34 +0800

    1

---
+++ packages/docs/src/views/tyhUi/backTop.vue
@@ -0,0 +1,7 @@
+<template>
+  <backTop-md v-high />
+</template>
+
+<script setup>
+  import backTopMd from '@/docs/backTop/backTop.md'
+</script>
---
+++ packages/docs/src/views/tyhUi/turnPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <turnPage-md v-high />
+</template>
+
+<script setup>
+  import turnPageMd from '@/docs/turnPage/turnPage.md'
+</script>

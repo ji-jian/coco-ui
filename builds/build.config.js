815f572a265412533d9abcd83d6f3c1b916d1b8c
bob <301085344@qq.com>
2022-06-23 23:25:50 +0800

    构建成功

--- builds/build.config.js
+++ builds/build.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+console.log(111)
 
 export default defineConfig({
+  base: '../',
   plugins: [vue()],
   build: {
     rollupOptions: {
-      input: '../packages/components/packages/button/index.ts',
+      input: {
+        button: './packages/components/packages/button/index.ts',
+        alert: './packages/components/packages/alert/index.ts'
+      },
       output: {
-        file: 'dist/bundle-button.js',
-        format: 'cjs'
+        dir: 'dist',
+        format: 'es'
       }
     }
   }

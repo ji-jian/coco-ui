63c78b501c32b69fb392ccae947adf94b14fa7ad
bob <301085344@qq.com>
2022-06-30 16:06:45 +0800

    1

--- internal/build/task.config.js
+++ internal/build/task.config.js
@@ -62,6 +62,16 @@ const buildAll = async () => {
         fileName: (format) => `dist/index.${format}.js`, // 输出文件名
         formats: ['umd', 'es', 'cjs']
       },
+      rollupOptions: {
+        // 确保外部化处理那些你不想打包进库的依赖,解决插件报错问题(reading 'isCE')
+        external: ['vue'],
+        output: {
+          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: {
+            vue: 'Vue'
+          }
+        }
+      },
       emptyOutDir: false,
       outDir: cocoLibRoot
     }
